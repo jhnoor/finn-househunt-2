178a93aeb928c7fbb8e25169b491968052872e7f
Jamawadi Noor <jamawadi@gmail.com>
2021-12-31 02:16:08 +0100

    env variable

--- google_sheet.py
+++ google_sheet.py
@@ -1,7 +1,13 @@
 import gspread
+import os
 
 def authenticate_google_sheet():
     # Authenticate with Google
-    gc = gspread.service_account()
+    credentials_file = os.environ['CLOUDSDK_AUTH_CREDENTIAL_FILE_OVERRIDE']
+    gc = gspread.service_account(filename='{}'.format(credentials_file))
     sh = gc.open("House Hunt 2: Return to Oslo")
     return sh.get_worksheet(2)
+
+
+# Get google credentials from environment variable
+
