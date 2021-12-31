24eedabe99e1d1e7cb8daedf308b7b93d15d07b7
Jamawadi Noor <jamawadi@gmail.com>
2021-12-31 02:09:33 +0100

    service account

--- google_sheet.py
+++ google_sheet.py
@@ -2,6 +2,6 @@ import gspread
 
 def authenticate_google_sheet():
     # Authenticate with Google
-    gc = gspread.oauth()
+    gc = gspread.service_account()
     sh = gc.open("House Hunt 2: Return to Oslo")
     return sh.get_worksheet(2)
