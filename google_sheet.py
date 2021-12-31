317cf22f5db624300b4cfb3ab0a14b4b2a00f2de
Jamawadi Noor <jamawadi@gmail.com>
2021-12-31 02:18:16 +0100

    Job done, now let's buy

--- google_sheet.py
+++ google_sheet.py
@@ -6,7 +6,7 @@ def authenticate_google_sheet():
     credentials_file = os.environ['CLOUDSDK_AUTH_CREDENTIAL_FILE_OVERRIDE']
     gc = gspread.service_account(filename='{}'.format(credentials_file))
     sh = gc.open("House Hunt 2: Return to Oslo")
-    return sh.get_worksheet(2)
+    return sh.get_worksheet(0)
 
 
 # Get google credentials from environment variable
